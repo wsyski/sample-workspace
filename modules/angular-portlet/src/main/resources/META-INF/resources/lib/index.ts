63fd94bcb176680a44689a6768a4fbbdaaa4e126
wsyski <wsyski@gmail.com>
2023-03-30 17:19:45 +0200

    Initial version

--- modules/angular-portlet/src/main/resources/META-INF/resources/lib/index.ts
+++ modules/angular-portlet/src/main/resources/META-INF/resources/lib/index.ts
@@ -4,9 +4,9 @@ declare var Liferay: any;
 
 export default function(rootId: string) {
 	Liferay.Loader.require(
-		'angular-portlet@1.0.0/lib/main',
-		(main: any) => {
-			main.default(rootId);
+		'angular-portlet@1.0.0/lib/bootstrap',
+		(bootstrap: any) => {
+			bootstrap.default(rootId);
 		},
 	);
 }
