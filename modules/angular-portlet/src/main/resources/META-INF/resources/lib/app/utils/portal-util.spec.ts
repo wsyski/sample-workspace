5b1dfc66a82c655f364e1062f6cad609e720467f
wsyski <wsyski@gmail.com>
2023-04-05 10:24:18 +0200

    Initial version

--- modules/angular-portlet/src/main/resources/META-INF/resources/lib/app/utils/portal-util.spec.ts
+++ modules/angular-portlet/src/main/resources/META-INF/resources/lib/app/utils/portal-util.spec.ts
@@ -3,16 +3,16 @@ import {PortalUtil} from './portal-util';
 describe('PortalUtil', () => {
 
 
-  describe('Portal Util functions', () => {
-    beforeEach(() => {
-    });
+    describe('Portal Util functions', () => {
+        beforeEach(() => {
+        });
 
-    it('getPortletId', () => {
-      expect(PortalUtil.getPortletId('_calendarEventList_INSTANCE_0QDW0O5Yb8m6_')).toEqual('calendarEventList_INSTANCE_0QDW0O5Yb8m6');
-    });
+        it('getPortletId', () => {
+            expect(PortalUtil.getPortletId('_calendarEventList_INSTANCE_0QDW0O5Yb8m6_')).toEqual('calendarEventList_INSTANCE_0QDW0O5Yb8m6');
+        });
 
-    it('getPortletName', () => {
-      expect(PortalUtil.getPortletName('_calendarEventList_INSTANCE_0QDW0O5Yb8m6_')).toEqual('calendarEventList');
+        it('getPortletName', () => {
+            expect(PortalUtil.getPortletName('_calendarEventList_INSTANCE_0QDW0O5Yb8m6_')).toEqual('calendarEventList');
+        });
     });
-  });
 });
--- modules/angular-portlet/src/main/resources/META-INF/resources/lib/index.ts
+++ modules/angular-portlet/src/main/resources/META-INF/resources/lib/index.ts
@@ -2,11 +2,11 @@ import './polyfills';
 
 declare var Liferay: any;
 
-export default function(rootId: string) {
-	Liferay.Loader.require(
-		'angular-portlet@1.0.0/lib/bootstrap',
-		(bootstrap: any) => {
-			bootstrap.default(rootId);
-		},
-	);
+export default function (rootId: string) {
+    Liferay.Loader.require(
+        'angular-portlet@1.0.0/lib/bootstrap',
+        (bootstrap: any) => {
+            bootstrap.default(rootId);
+        },
+    );
 }
